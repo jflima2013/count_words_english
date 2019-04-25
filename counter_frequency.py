5735186abe40a00454f4328b1be542ff83c484c3
JFranklin Lima <jfranklincontatos@gmail.com>
2019-04-25 15:33:32 -0300

    criado o programa principal

--- counter_frequency.py
+++ counter_frequency.py
@@ -3,16 +3,8 @@ class Counter(dict):
         return 0
 
 
-word_list = ['ola', 'ola', 'boa', 'boa', 'bondadade', 'amor', 'diginidade',
-             'amor', 'boa', 'justiça', 'liberdade', 'paciência', 'paciência']
-
-
 def cont_frequency(word_list):
     counts = Counter()
     for word in word_list:
         counts[word] += 1
     return counts
-
-
-counts = cont_frequency(word_list)
-print(counts)
---
+++ prog.py
@@ -0,0 +1,7 @@
+from counter_frequency import cont_frequency
+
+word_list = ['ola', 'ola', 'boa', 'boa', 'bondadade', 'amor', 'diginidade',
+             'amor', 'boa', 'justiça', 'liberdade', 'paciência', 'paciência']
+
+counts = cont_frequency(word_list)
+print(counts)
