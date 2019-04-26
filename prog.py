dbef029adccd75b4d168047ca9fe7ec382aa6d2f
JFranklin Lima <jfranklincontatos@gmail.com>
2019-04-26 15:19:35 -0300

    já consegue ordenar o dict counts

--- prog.py
+++ prog.py
@@ -7,6 +7,6 @@ word_list = ['ola', 'ola', 'boa', 'boa', 'bondadade', 'amor', 'diginidade',
 
 counts = cont_frequency(word_list)
 
-for k, v in counts.items():
-    k, v = v, k
+for count in sorted(counts, key=counts.get, reverse=True):
+    k, v = count, counts[count]
     print("%s: %s" % (k, v))
