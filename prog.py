2231aba38fd62b76f0a719caf3568599230c44ea
JFranklin Lima <jfranklincontatos@gmail.com>
2019-04-26 18:56:27 -0300

    o programa agora cria lista de palavras para serem contadas

--- prog.py
+++ prog.py
@@ -1,9 +1,12 @@
 from counter_frequency import cont_frequency
+from words_from_strings import words_from_string
 
 text = "But I didn't inhale, he said (emphatically)"
 
 word_list = ['ola', 'ola', 'boa', 'boa', 'bondadade', 'amor', 'diginidade',
              'amor', 'boa', 'justiça', 'liberdade', 'paciência', 'paciência']
+
+word_list = words_from_string(text)
 
 counts = cont_frequency(word_list)
 
