8ab32c8f679eff7571ce33e7e2de95dc48dce0dc
JFranklin Lima <jfranklincontatos@gmail.com>
2019-04-26 18:52:32 -0300

    add biblioteca nltk para resolver o problema de word contractions

--- prog.py
+++ prog.py
@@ -7,6 +7,6 @@ word_list = ['ola', 'ola', 'boa', 'boa', 'bondadade', 'amor', 'diginidade',
 
 counts = cont_frequency(word_list)
 
-for count in sorted(counts, key=counts.get, reverse=True):
-    k, v = count, counts[count]
-    print("%s: %s" % (k, v))
+for word in sorted(counts, key=counts.get, reverse=True):
+    k, v = word, counts[word]
+    print("%s: %s" % (v, k))
--- words_from_strings.py
+++ words_from_strings.py
@@ -1,10 +1,12 @@
-import re
+import nltk
 
 
-def words_from_string(string):
-    string = string.lower()
-    return re.split(r"\w'+", string)
+def words_from_string(text):
+    text = text.lower()
+    return nltk.word_tokenize(text)
 
 
-str = "But I didn't inhale, he said (emphatically)"
-print(words_from_string(str))
+# str = "hello seattle what have you got"
+# str += "But I didn't inhale, he said (emphatically)"
+# str += "I wouldn't've done that."
+# print(words_from_string(str))
