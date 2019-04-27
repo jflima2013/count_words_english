2e7a2da69f7f475c9046a4ab6022400729d2b3a7
JFranklin Lima <jfranklincontatos@gmail.com>
2019-04-26 21:58:15 -0300

    ler um arquivo e cria uma lisata de palavras a partir deste arquivo

--- prog.py
+++ prog.py
@@ -3,8 +3,11 @@ from words_from_strings import words_from_string
 
 text = "But I didn't inhale, he said (emphatically)"
 
-word_list = ['ola', 'ola', 'boa', 'boa', 'bondadade', 'amor', 'diginidade',
-             'amor', 'boa', 'justiça', 'liberdade', 'paciência', 'paciência']
+file_name = '/home/jfk/pickaxe/tut_containers/word_freq/books/\
+programming_ruby.txt'
+
+with open(file_name, 'r') as reader:
+    text = reader.read()
 
 word_list = words_from_string(text)
 
@@ -12,4 +15,4 @@ counts = cont_frequency(word_list)
 
 for word in sorted(counts, key=counts.get, reverse=True):
     k, v = word, counts[word]
-    print("%s: %s" % (v, k))
+    # print("%s: %s" % (v, k))
