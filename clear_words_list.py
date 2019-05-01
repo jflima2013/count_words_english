cb7a97aa8f91fd7fedb29f110a3f75e172c5fed9
JFranklin Lima <jfranklincontatos@gmail.com>
2019-05-01 00:34:00 -0300

    usando as tuplas em sequências que não irão mudar

--- clear_words_list.py
+++ clear_words_list.py
@@ -2,30 +2,30 @@ import string
 from itertools import chain
 
 
-punctuations = list(string.punctuation)
-digits = list(string.digits)
+punctuations = tuple(string.punctuation)
+digits = tuple(string.digits)
 
-key_words = [
+key_words = (
     "False", "await", "else", "import", "pass", "None", "break",
     "except", "in", "raise", "True", "class", "finally", "is",
     "return", "and", "continue", "for", "lambda", "try", "as",
     "def", "from", "nonlocal", "while", "assert", "del",
     "global", "not", "with", "async", "elif", "if", "or",
     "yield", "def"
-    ]
+    )
 
-unwanted_words = ["’"]
+unwanted_words = ("’")
 words_already_known = []
 
 
-words_not_allowed = punctuations + digits + key_words + unwanted_words + \
-                      words_already_known
+# words_not_allowed = punctuations + digits + key_words + unwanted_words + \
+#                       words_already_known
 
 # words_not_allowed = punctuations, digits, key_words, unwanted_words, \
 #                       words_already_known
 
-# words_not_allowed = chain(punctuations, digits, key_words, unwanted_words,
-#                           words_already_known)
+words_not_allowed = chain(punctuations, digits, key_words, unwanted_words,
+                          words_already_known)
 
 
 def clear_word_list(words_list):
